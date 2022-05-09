adef857c7bbfd8af147bb74b73cdf445bc3fdf51
Job85 <julianjernigan@ymail.com>
2022-05-08 21:28:00 -0400

    deleted walletId, added constraints to locationId, entered funtion for up & down async

---
+++ backend/migrations/20220509010148-create-token.js
@@ -0,0 +1,35 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('tokens', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      code: {
+        type: Sequelize.STRING
+      },
+      locationId: {
+        type: Sequelize.INTEGER,
+        onDelete: '',
+        references: {
+          model: 'locations',
+          key: 'id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('tokens');
+  }
+};
