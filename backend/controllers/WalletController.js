4555d09531146663460b6a4ec287aec49510c529
Job85 <julianjernigan@ymail.com>
2022-05-09 13:55:35 -0400

    created function and route for GetWallet

--- backend/controllers/WalletController.js
+++ backend/controllers/WalletController.js
@@ -1,19 +1,29 @@
 const { Wallet, Token } = require('../models')
 
-
+const GetWallet = async (req, res) => {
+    try {
+        const wallet = await Wallet.findAll({
+            attributes: ['ownerId']
+        })
+        console.log(wallet)
+        res.send(wallet)
+    } catch (error) {
+        throw error
+    }
+}
 const GetMyWallet = async (req, res) => {
     try {
         const wallet = await Wallet.findAll({
             attributes: ['ownerId'],
-            include: [{
-                model: Token,
-                as: 'owner',
-                // through: { attributes: [] },
-                // attributes: ['id', 'code']
-                where: {
-                    id: req.params.ownerId
-                }
-            }],
+            // include: [{
+            //     model: Token,
+            //     as: 'token',
+            //     through: { attributes: [] },
+            //     attributes: ['id', 'code']
+            // where: {
+            //     userd: req.params.user_id
+            // }
+            // }],
         })
         console.log(wallet)
         res.send(wallet)
@@ -22,14 +32,8 @@ const GetMyWallet = async (req, res) => {
     }
 }
 
-// const GetMyWallet = async (req, res) => {
-//     try {
-//         const myWallet = await Wallet.findByPk()
-//     } catch (error) {
-//         throw error
-//     }
-// }
 
 module.exports = {
+    GetWallet,
     GetMyWallet,
 }
