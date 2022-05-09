a62752dc19acf2e7991ee946f915285d30eefcb2
Job85 <julianjernigan@ymail.com>
2022-05-09 01:28:50 -0400

    created file, entered logic for up & down async, entered seed

---
+++ backend/seeders/20220509052327-demo-location.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Locations', [{
+      latitude: 45.900890,
+      longitude: 98.073943,
+      level: 'Easy',
+      userId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Locations', null, {});
+  }
+};
