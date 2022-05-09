3bc63f05e7711eec2f41d6937f124bc5694ba2e3
Job85 <julianjernigan@ymail.com>
2022-05-09 12:32:31 -0400

    changed GetWallet to GetMyWallet and added logic to find wallet with attributes, include, where

--- backend/controllers/WalletController.js
+++ backend/controllers/WalletController.js
@@ -1,8 +1,18 @@
 const { Wallet } = require('../models')
 
-const GetWallet = async (req, res) => {
+const GetMyWallet = async (req, res) => {
     try {
-        const wallet = await Wallet.findAll()
+        const wallet = await Wallet.findAll({
+            attributes: ['ownerId'],
+            include: [{
+                model: Token,
+                through: { attributes: [] },
+                attributes: ['id', 'code']
+            }],
+            where: {
+                id: req.params.ownerId
+            }
+        })
         console.log(wallet)
         res.send(wallet)
     } catch (error) {
@@ -10,7 +20,14 @@ const GetWallet = async (req, res) => {
     }
 }
 
+// const GetMyWallet = async (req, res) => {
+//     try {
+//         const myWallet = await Wallet.findByPk()
+//     } catch (error) {
+//         throw error
+//     }
+// }
 
 module.exports = {
-    GetWallet,
+    GetMyWallet,
 }
