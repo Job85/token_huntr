bc00fb3fead59ce5dce0d403f28b8a1ecbb5629d
Job85 <julianjernigan@ymail.com>
2022-05-10 14:16:42 -0400

    added LocationReducer to store

--- token-huntr-app/src/store/index.js
+++ token-huntr-app/src/store/index.js
@@ -1,5 +1,7 @@
 import { legacy_createStore as createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import LocationReducer from './reducers/LocationReducer'
 
-const store = createStore(() => ({}))
+const store = createStore(LocationReducer, composeWithDevTools())
 
 export default store
