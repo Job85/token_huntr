80ac237708f5c44454b245c89741f91f3ebd02c3
Job85 <julianjernigan@ymail.com>
2022-05-10 14:08:35 -0400

    exported GetLocationDetails function

--- token-huntr-app/src/services/LocationServices.js
+++ token-huntr-app/src/services/LocationServices.js
@@ -1,9 +1,19 @@
 import TokenHuntr from ".";
 
-export const getLocations = async () => {
+export const GetLocations = async () => {
     try {
         const res = await TokenHuntr.get('/locations')
         console.log(res, 'getting locations')
+        return res.data
+    } catch (error) {
+        throw error
+    }
+}
+
+export const GetLocationDetails = async (id) => {
+    try {
+        const res = await TokenHuntr.get(`/locations/${id}`)
+        console.log(res.data)
         return res.data
     } catch (error) {
         throw error
