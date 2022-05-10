ca9232c6d6b30bd26ac8e2928af6aa2edfa14272
Job85 <julianjernigan@ymail.com>
2022-05-09 23:23:28 -0400

    provided store to pass props

--- client/token-huntr-app/src/index.js
+++ client/token-huntr-app/src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store'
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
 
