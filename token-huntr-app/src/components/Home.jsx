679fedc438c4f16db91c0a006e293b083ac3b64d
Job85 <julianjernigan@ymail.com>
2022-05-10 10:24:15 -0400

    renamed file, added h1 tag and h3 tag w/link to locations

---
+++ token-huntr-app/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+    return (
+        <div>
+            <h1>
+                Welcome to New Adventures
+            </h1>
+            <h3>
+                <Link to={'/locations'}> Onward to New Heights</Link>
+            </h3>
+        </div>
+    )
+}
