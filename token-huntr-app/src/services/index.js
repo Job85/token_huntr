d69b622c52da88513301a3c1fcfe4d6fa57d8e40
Job85 <julianjernigan@ymail.com>
2022-05-11 12:49:44 -0400

    updated with auth token axios call

--- token-huntr-app/src/services/index.js
+++ token-huntr-app/src/services/index.js
@@ -1,6 +1,20 @@
 import Axios from 'axios'
 
+
+export const BASE_URL = process.env.NODE_ENV === 'local' ? 'http://localhose:3001/api' : 'https://token-huntr.com/api'
+console.log(BASE_URL, "You're in BASE_URL")
 const TokenHuntr = Axios.create({ baseURL: 'http://localhost:3001' })
 
+TokenHuntr.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token')
+        if (token) {
+            config.headers['authorization'] = `Bearer ${token}`
+            config.headers['access-control-allow-origin'] = '*'
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+)
 
 export default TokenHuntr
--- token-huntr-app/src/store/actions/LocationActions.js
+++ token-huntr-app/src/store/actions/LocationActions.js
@@ -1,36 +1,36 @@
-import { GetLocations } from "../../services/LocationServices";
-import { GET_LOCATIONS, ADD_LOCATION, NEW_LOCATION, UPDATE_LOCATION, REMOVE_LOCATION } from "../types";
+// import { GetLocations } from "../../services/LocationServices";
+// import { GET_LOCATIONS, ADD_LOCATION, NEW_LOCATION, UPDATE_LOCATION, REMOVE_LOCATION } from "../types";
 
-export const LoadLocations = () => {
-    return async (dispatch) => {
-        try {
-            const locations = await GetLocations()
-            dispatch({
-                type: GET_LOCATIONS,
-                payload: locations
-            })
-        } catch (error) {
-            throw error
-        }
-    }
-}
+// export const LoadLocations = () => {
+//     return async (dispatch) => {
+//         try {
+//             const locations = await GetLocations()
+//             dispatch({
+//                 type: GET_LOCATIONS,
+//                 payload: locations
+//             })
+//         } catch (error) {
+//             throw error
+//         }
+//     }
+// }
 
-export const AddLocation = (location) => ({
-    type: ADD_LOCATION,
-    payload: location
-})
+// export const AddLocation = (location) => ({
+//     type: ADD_LOCATION,
+//     payload: location
+// })
 
-export const CreateNewLocation = (formValue) => ({
-    type: NEW_LOCATION,
-    payload: formValue
-})
+// export const CreateNewLocation = (formValue) => ({
+//     type: NEW_LOCATION,
+//     payload: formValue
+// })
 
-export const UpdateLocation = (formValue) => ({
-    type: UPDATE_LOCATION,
-    payload: formValue
-})
+// export const UpdateLocation = (formValue) => ({
+//     type: UPDATE_LOCATION,
+//     payload: formValue
+// })
 
-export const RemoveLocation = (index) => ({
-    type: REMOVE_LOCATION,
-    payload: index
-})
+// export const RemoveLocation = (index) => ({
+//     type: REMOVE_LOCATION,
+//     payload: index
+// })
