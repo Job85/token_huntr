022e1d5795469b5817c203874c5e6bba56399e75
Job85 <julianjernigan@ymail.com>
2022-05-11 18:05:17 -0400

    added register, login, logout & error routes

--- token-huntr-app/src/App.js
+++ token-huntr-app/src/App.js
@@ -1,37 +1,19 @@
-import { Route, Routes } from 'react-router-dom'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Toolbar, Box } from '@mui/material';
 import Nav from './components/Nav';
-import Landing from './pages/LandingPage'
-import Register from './pages/Register';
-import Login from './pages/Login.js'
-import Home from './components/Home'
+import Error from './components/Error';
+import Landing from './pages/LandingPage';
+import Register from './components/Register';
+import Login from './components/Login.js';
+import Home from './components/Home';
 import LocationList from './components/LocationList';
 // import LocationForm from './components/LocationForm';
 import './App.css';
-import { CheckSession } from './services/Auth';
-import { useState, useEffect } from 'react';
+
 
 const App = () => {
-  const [authenticated, toggleAuthenticated] = useState(false)
-  const [user, setUser] = useState(null)
 
-  const checkToken = async () => {
-    const user = await CheckSession();
-    setUser(user);
-    toggleAuthenticated(true);
-  }
-
-  const handleLogOut = () => {
-    setUser(null)
-    toggleAuthenticated(false)
-    localStorage.clear()
-  }
-
-  useEffect(() => {
-    const token = localStorage.getItem('token')
-    if (token) {
-      checkToken()
-    }
-  }, [])
 
   return (
     <div className="App">
@@ -42,17 +24,25 @@ const App = () => {
           handleLogOut={handleLogOut}
         />
       </header>
+      <Toolbar />
+      <Box className={loginBox}>
+        <Switch>
+          <Route path='/' element={<Landing />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/logout' render={(routerProps) => <wrappedComponents.AuthenticatedLogout protected {...routerProps} />} />
+          <Route element={Error} />
+        </Switch>
+      </Box>
       <h1>
         Token Huntr
       </h1>
       <div>
         <Routes>
-          <Route path='/' element={<Landing />} />
-          <Route path='/auth/register' element={<Register />} />
-          <Route path='/login' element={<Login
-            setUser={setUser}
-            toggleAuthenticated={toggleAuthenticated} />} />
-          <Route path='/home' element={<Home />} />
+
+
+
           <Route path='/locations' element={<LocationList
             user={user}
             authenticated={authenticated} />} />
@@ -64,3 +54,28 @@ const App = () => {
 }
 
 export default App;
+
+
+// import { CheckSession } from './services/Auth';
+// import { useState, useEffect } from 'react';
+// const [authenticated, toggleAuthenticated] = useState(false)
+// const [user, setUser] = useState(null)
+
+// const checkToken = async () => {
+//   const user = await CheckSession();
+//   setUser(user);
+//   toggleAuthenticated(true);
+// }
+
+// const handleLogOut = () => {
+//   setUser(null)
+//   toggleAuthenticated(false)
+//   localStorage.clear()
+// }
+
+// useEffect(() => {
+//   const token = localStorage.getItem('token')
+//   if (token) {
+//     checkToken()
+//   }
+// }, [])
