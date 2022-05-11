12d5876e1e1beb80a68334ee5f96f39e8afed70d
Job85 <julianjernigan@ymail.com>
2022-05-11 10:42:08 -0400

    added logic to register user

---
+++ token-huntr-app/src/pages/Register.js
@@ -0,0 +1,110 @@
+import React from 'react'
+import { useState } from 'react'
+import { RegisterUser } from '../services/Auth'
+import { useNavigate } from 'react-router-dom'
+
+const Register = () => {
+    let navigate = useNavigate()
+
+    const [formValues, setFormValues] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    })
+
+    const handleChange = (e) => {
+        setFormValues({ ...formValues, [e.target.name]: e.target.value })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        await RegisterUser({
+            firstName: formValues.firstName,
+            lastName: formValues.lastName,
+            email: formValues.email,
+            password: formValues.password
+        })
+        setFormValues({
+            firstName: '',
+            lastName: '',
+            email: '',
+            password: '',
+            confirmPassword: ''
+        })
+        navigate('/signin')
+    }
+
+    return (
+        <div>
+            <form className="col" onSubmit={handleSubmit}>
+                <div className="input-wrapper">
+                    <label htmlFor="firstName">First Name</label>
+                    <input
+                        onChange={handleChange}
+                        name="firstName"
+                        type="text"
+                        placeholder="Joe"
+                        value={formValues.firstName}
+                        required
+                    />
+                </div>
+                <div className="input-wrapper">
+                    <label htmlFor="lastName">Last Name</label>
+                    <input
+                        onChange={handleChange}
+                        name="lastName"
+                        type="text"
+                        placeholder="Shmoe"
+                        value={formValues.lastName}
+                        required
+                    />
+                </div>
+                <div className="input-wrapper">
+                    <label htmlFor="email">Email</label>
+                    <input
+                        onChange={handleChange}
+                        name="email"
+                        type="email"
+                        placeholder="example@example.com"
+                        value={formValues.email}
+                        required
+                    />
+                </div>
+
+                <div className="input-wrapper">
+                    <label htmlFor="password">Password</label>
+                    <input
+                        onChange={handleChange}
+                        type="password"
+                        name="password"
+                        value={formValues.password}
+                        required
+                    />
+                </div>
+                <div className="input-wrapper">
+                    <label htmlFor="confirmPassword">Confirm Password</label>
+                    <input
+                        onChange={handleChange}
+                        type="password"
+                        name="confirmPassword"
+                        value={formValues.confirmPassword}
+                        required
+                    />
+                </div>
+                <button
+                    disabled={
+                        !formValues.firstName ||
+                        !formValues.lastName ||
+                        !formValues.email ||
+                        (!formValues.password &&
+                            formValues.confirmPassword === formValues.password)
+                    }
+                >
+                    Register
+                </button>
+            </form>
+        </div>
+    )
+}
