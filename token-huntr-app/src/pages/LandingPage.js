4b988a8e1c4778837ed96269d321dcfff1ef3bde
Job85 <julianjernigan@ymail.com>
2022-05-12 10:32:28 -0400

    removed auth from paths

--- token-huntr-app/src/pages/LandingPage.js
+++ token-huntr-app/src/pages/LandingPage.js
@@ -9,10 +9,10 @@ const Landing = () => {
             <section>
                 <h1>Welcome to Token Huntr</h1>
                 <h2>Log In or Sign Up for New Adventures</h2>
-                <button onClick={() => navigate('/auth/signin')}>
+                <button onClick={() => navigate('/signin')}>
                     Log In Here
                 </button>
-                <button onClick={() => navigate('/auth/register')}>
+                <button onClick={() => navigate('/register')}>
                     Sign Up Here
                 </button>
             </section>
