dd14d9b9baff5d4762e4588c5291a0cc51bba800
Job85 <julianjernigan@ymail.com>
2022-05-12 10:09:05 -0400

    setup location form for react from redux

--- token-huntr-app/src/components/LocationForm.jsx
+++ token-huntr-app/src/components/LocationForm.jsx
@@ -1,43 +1,85 @@
-import React, { useReducer } from 'react';
-import LocationReducer from '../store/reducers/LocationReducer';
+import React, { useState } from 'react';
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+// import LocationReducer from '../store/reducers/LocationReducer';
 
 
-const LocationForm = (props) => {
-    console.log(props)
+const CreateLocation = ({ user }) => {
+    let navigate = useNavigate()
+    const user_id = user.id
+
+    const [formValues, setFormValues] = useState({
+        latitude: '',
+        longitude: '',
+        level: ''
+    })
+
+    const handleChange = (e) => {
+        setFormValues({ ...formValues, [e.target.name]: e.target.value })
+    }
+
+    const CreateCache = async () => {
+        let url = process.env.NODE_ENV === 'local' ? `http://localhost:3001/api/post/${user_id}` : `https://token-huntr-app.herokuapp.com/api/post/${user_id}`
+        await axios({
+            url,
+            method: 'post',
+            data: formValues
+        })
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        CreateCache()
+        setFormValues({
+            latitude: '',
+            longitude: '',
+            level: ''
+        })
+        navigate('/locations')
+        window.location.reload(false)
+    }
+
     return (
         <div className='local-form'>
             <h2>Add a New Geocache</h2>
             <h4>Fill out form below!</h4>
-            <form onSubmit={props.handleSubmit}>
+            <form onSubmit={handleSubmit}>
                 <ul>
                     <ul>
                         <label>Latitude:</label>
                         <input type='text'
                             placeholder={'Latitude'}
-                            name='newLocation'
-                            // value={props.newLocation.latitude}
-                            onChange={props.handleChange}
+                            name='latitude'
+                            value={formValues.latitude}
+                            onChange={handleChange}
+                            required
                         />
                     </ul>
                     <ul>
                         <label>Longitude:</label>
                         <input type='text'
                             placeholder={'Longitude'}
-                            name='newLocation'
-                            value={props.newLocation.longitude}
-                            onChange={props.handleChange}
+                            name='longitude'
+                            value={formValues.longitude}
+                            onChange={handleChange}
                         />
                     </ul>
                     <ul>
                         <label>Level:</label>
                         <input type='text'
-                            placeholder={'Level'}
-                            name='newLocation'
-                            value={props.newLocation.level}
-                            onChange={props.handleChange}
+                            placeholder={'Easy, Moderate, Hard'}
+                            name='level'
+                            value={formValues.level}
+                            onChange={handleChange}
                         />
                     </ul>
-                    <button type='submit' onClick={props.handleSubmit}>
+                    <button
+                        disabled={
+                            !formValues.latitude ||
+                            !formValues.longitude ||
+                            !formValues.level
+                        }
+                    >
                         Add Cache
                     </button>
                 </ul>
@@ -46,4 +88,4 @@ const LocationForm = (props) => {
     )
 }
 
-export default LocationForm
+export default CreateLocation
