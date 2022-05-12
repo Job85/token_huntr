22135eba3ef6dcbff135ab1c6253f1eef400b509
Job85 <julianjernigan@ymail.com>
2022-05-12 10:28:44 -0400

    muted code

--- token-huntr-app/src/store/reducers/LocationReducer.js
+++ token-huntr-app/src/store/reducers/LocationReducer.js
@@ -1,40 +1,40 @@
-const { ADD_LOCATION, LOAD_LOCATION, GET_LOCATIONS, NEW_LOCATION, UPDATE_LOCATION, REMOVE_LOCATION } = require('../types')
+// const { ADD_LOCATION, LOAD_LOCATION, GET_LOCATIONS, NEW_LOCATION, UPDATE_LOCATION, REMOVE_LOCATION } = require('../types')
 
-const iState = {
-    locations: [],
-    newLocation: { latitude: '' },
-    newLocation: { longitude: '' },
-    newLocation: { level: '' },
-    updateLocation: { latitude: '' },
-    updateLocation: { longitude: '' },
-    updateLocation: { level: '' },
-    locationsLoading: ''
-}
+// const iState = {
+//     locations: [],
+//     newLocation: { latitude: '' },
+//     newLocation: { longitude: '' },
+//     newLocation: { level: '' },
+//     updateLocation: { latitude: '' },
+//     updateLocation: { longitude: '' },
+//     updateLocation: { level: '' },
+//     locationsLoading: ''
+// }
 
-const LocationReducer = (state = iState, action) => {
-    switch (action.type) {
-        case LOAD_LOCATION:
-            return { ...state, locationsLoading: action.payload }
-        case GET_LOCATIONS:
-            state.locations = new Array(...action.payload)
-            return { ...state }
-        case ADD_LOCATION:
-            return {
-                ...state, locations: [...state.locations, action.payload],
-                newLocation: { ...state.newLocation, latitude: '', longitude: '', level: '' }
-            }
-        case NEW_LOCATION:
-            return { ...state, newLocation: { ...state.newLocation, } }
-        case UPDATE_LOCATION:
-            return {
-                ...state, locations: [...state.locations, action.payload],
-                updateLocation: { ...state.updateLocation, latitude: '', longitude: '', level: '' }
-            }
-        case REMOVE_LOCATION:
-            state.locations.splice(action.payload, 1)
-        default:
-            return { ...state }
-    }
-}
+// const LocationReducer = (state = iState, action) => {
+//     switch (action.type) {
+//         case LOAD_LOCATION:
+//             return { ...state, locationsLoading: action.payload }
+//         case GET_LOCATIONS:
+//             state.locations = new Array(...action.payload)
+//             return { ...state }
+//         case ADD_LOCATION:
+//             return {
+//                 ...state, locations: [...state.locations, action.payload],
+//                 newLocation: { ...state.newLocation, latitude: '', longitude: '', level: '' }
+//             }
+//         case NEW_LOCATION:
+//             return { ...state, newLocation: { ...state.newLocation, } }
+//         case UPDATE_LOCATION:
+//             return {
+//                 ...state, locations: [...state.locations, action.payload],
+//                 updateLocation: { ...state.updateLocation, latitude: '', longitude: '', level: '' }
+//             }
+//         case REMOVE_LOCATION:
+//             state.locations.splice(action.payload, 1)
+//         default:
+//             return { ...state }
+//     }
+// }
 
-export default LocationReducer
+// export default LocationReducer
